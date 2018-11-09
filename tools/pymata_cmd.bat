3855679a6cff86dd91d8f4a599da65dacbd7346f
SebCanet <scanet@libreduc.cc>
2018-11-09 22:01:13 +0100

    update B@electron

--- tools/pymata_cmd.bat
+++ tools/pymata_cmd.bat
@@ -4,4 +4,4 @@ rem fichiers BAT et fork créés par Sébastien CANET
 SET currentpath=%~dp1
 cd %currentpath%tools\python
 cls
-python.exe .\Lib\site-packages\pymata_aio\pymata_iot.py -l 5 -c no_client -comport COM13
+python.exe .\Lib\site-packages\pymata_aio\pymata_iot.py -l 5 -c no_client -comport COM33
